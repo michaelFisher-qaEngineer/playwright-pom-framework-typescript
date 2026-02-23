1fb11d56a8342452d8dad9e217a5e349d29a8f75
Michael Fisher <michael.fisher.qaengineer@gmail.com>
2026-02-23 10:19:15 -0800

    adding missing regression tag

--- tests/TC01_LaunchApplication.spec.ts
+++ tests/TC01_LaunchApplication.spec.ts
@@ -1,10 +1,8 @@
 import { test } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 
-test('@sanity @TC01 TC01_LaunchApplication', async ({page}) => {
-
+test('@sanity @regression @TC01 TC01_LaunchApplication', async ({page}) => {
     const home = new HomePage(page);
     await home.openHomePage();
     await home.verifyHomePageTitle();
-
 });
